3fefba6e49a1eb84edd36ab51f6b9c947a1464d5
“Kilig” <syp01150415@gmail.com>
2023-01-04 15:01:36 +0800

    Update xinlang.py

--- SpiderPractice/spiders/xinlang.py
+++ SpiderPractice/spiders/xinlang.py
@@ -1,38 +1,57 @@
 import scrapy
+from urllib.parse import urlencode
 from SpiderPractice.items import XinLangItem
 
 class XinlangSpider(scrapy.Spider):
     name = 'xinlang'
     allowed_domains = ['vip.stock.finance.sina.com.cn']
-    start_urls = ['https://vip.stock.finance.sina.com.cn/q/go.php/vComStockHold/kind/jgcg/index.phtml?num=40&p=1']
+    
+    def start_requests(self):
+        dev_url = 'https://vip.stock.finance.sina.com.cn/q/go.php/vComStockHold/kind/jgcg/index.phtml'
+        page = 1
+        parse = {
+            "num":40,
+            "p":page
+        }
+        url = dev_url + '?' + urlencode(parse)
+        yield scrapy.Request(
+            url=url,
+            callback=self.parse,
+            meta={
+                "dev_url":dev_url,
+                'parse':parse
+            }
+        )
 
     def parse(self, response):
-        ID = response.xpath("//*[@id='dataTable']//tr//td[1]/a/text()").extract()
-        name = response.xpath("//*[@id='dataTable']//tr//td[2]/a/text()").extract()
-        count = response.xpath("//*[@id='dataTable']//tr//td[3]/text()").extract()
-        CoCh = response.xpath("//*[@id='dataTable']//tr//td[4]/text()").extract()
-        HoRatio = response.xpath("//*[@id='dataTable']//tr//td[5]/text()").extract()
-        InchoRatio = response.xpath("//*[@id='dataTable']//tr//td[6]/text()").extract()
-        PerTra = response.xpath("//*[@id='dataTable']//tr//td[7]/text()").extract()
-        InPerTra = response.xpath("//*[@id='dataTable']//tr//td[8]/text()").extract()
-        for i in range(len(name)):
+
+        tds = response.xpath("//table[@id='dataTable']//tr[not(@style = 'display: none')]")
+
+        for td in tds[1:]:
             item = XinLangItem()
-            item["ID"] = ID[i]
-            item["name"] = name[i]
-            item["count"] = count[i]
-            item["CoCh"] = CoCh[i]
-            item["HoRatio"] = HoRatio[i]
-            item["InchoRatio"] = InchoRatio[i]
-            item["PerTra"] = PerTra[i]
-            item["InPerTra"] = InPerTra[i]
+            item["ID"] = td.xpath('./td[1]/a/text()').get()
+            item["name"] = td.xpath("./td[2]/a/text()").get()
+            item["count"] = td.xpath("./td[3]/text()").get()
+            item["CoCh"] = td.xpath("./td[4]/text()").get()
+            item["HoRatio"] = td.xpath("./td[5]/text()").get()
+            item["InchoRatio"] = td.xpath("./td[6]/text()").get()
+            item["PerTra"] = td.xpath("./td[7]/text()").get()
+            item["InPerTra"] = td.xpath("./td[8]/text()").get()
             print("ID：%s, 名称：%s"%(item["ID"], item["name"]))
             yield item
         
-        page = int(response.url.split('=')[-1]) + 1
-        page_class = response.xpath('/html/body/div[1]/div[4]/div[2]/div/div[1]/div[5]/a[8]/@class').extract_first()
+        page_class = response.xpath('/html/body/div[1]/div[4]/div[2]/div/div[1]/div[5]/a[8]/@class').get()
         if page_class == 'page':
-            next_url = f"https://vip.stock.finance.sina.com.cn/q/go.php/vComStockHold/kind/jgcg/index.phtml?num=40&p={page}"
+            dev_url = response.meta['dev_url']
+            page = response.meta['parse']['p'] + 1
+            parse = response.meta['parse']
+            parse['p'] = page
+            url = dev_url + '?' + urlencode(parse)
             yield scrapy.Request(
-                url=next_url,
-                callback=self.parse
+                url=url,
+                callback=self.parse,
+                meta={
+                "dev_url":dev_url,
+                'parse':parse
+            }
             )
